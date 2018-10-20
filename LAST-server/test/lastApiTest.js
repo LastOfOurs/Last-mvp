1ae5560801e8551f4449c8720cb6a1b7578a29de
Pong Cheecharern <k.cheecharern@gmail.com>
2018-10-20 16:21:36 +0700

    got kubectl and minikube to run with yml file

--- LAST-server/test/lastApiTest.js
+++ LAST-server/test/lastApiTest.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'test';
 const loopbackApiTesting = require('loopback-api-testing')
 const tests = require('./lastTestConfig.json')
 const server = require('../server/server.js')
-const url = 'http://localhost:8090/api'
+const url = 'http://0.0.0.0:3000/api'
 
 loopbackApiTesting.run(tests, server, url, function(err) {
   if (err) {
--- scripts/last-clear.sh
+++ scripts/last-clear.sh
@@ -2,9 +2,9 @@
 #nuke all animals
 printf "======= removing animals and IPFS data \n"
 
-curl -X DELETE http://localhost:8090/api/animals/1 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
-curl -X DELETE http://localhost:8090/api/animals/2 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
-curl -X DELETE http://localhost:8090/api/animals/3 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
+curl -X DELETE http://0.0.0.0:3000/api/animals/1 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
+curl -X DELETE http://0.0.0.0:3000/api/animals/2 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
+curl -X DELETE http://0.0.0.0:3000/api/animals/3 -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' 
 
 #nuke your IPFS data store
 
