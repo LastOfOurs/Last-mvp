73550c1834f863486160d4a8cd2cd8cf47028493
Pong Cheecharern <k.cheecharern@gmail.com>
2018-11-03 22:39:23 +0100

    deploying egg contract with hatch function

--- LAST-contract/contracts/Egg.sol
+++ LAST-contract/contracts/Egg.sol
@@ -5,4 +5,18 @@ contract Egg is MintableToken {
     string public constant name = "Basic LAST Egg";
     string public constant symbol = "EGG";
     uint8 public constant decimals = 0;
+
+    event Hatching(address recipient, uint256 amount, uint256 timestamp);
+
+    /**
+   * @dev Function to hatch an egg (sends eggs to zero address)
+   * @param amount amount of eggs to hatch
+   * @return A boolean that indicates if the operation was successful.
+   */
+    function hatch(uint amount) public returns (bool){
+        require(amount <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(amount);
+        emit Hatching(msg.sender, amount, block.timestamp);
+        return true;
+    }
 }
--- LAST-contract/migrations/2_deploy_contract.1.js
+++ LAST-contract/migrations/2_deploy_contract.1.js
@@ -1,5 +1,7 @@
 var LAST = artifacts.require('./LAST.sol')
+var Egg = artifacts.require('./Egg.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(LAST, 'LAST', 'LTK', { gas: 5000000 })
+  deployer.deploy(Egg)
 }
