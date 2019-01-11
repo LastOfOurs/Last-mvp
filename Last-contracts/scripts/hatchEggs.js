695dd2b5d61b8c8f4d735f6c631def0e3dbffb79
mhaii <bloo20xd@gmail.com>
2019-01-11 16:04:17 +0700

    make hatch function accept a specific amount instead of 1

--- Last-contracts/scripts/hatchEggs.js
+++ Last-contracts/scripts/hatchEggs.js
@@ -12,9 +12,9 @@ function sleep(milliseconds) {
 module.exports = async () => {
   try {
     let eggContract = await Egg.deployed()
-    let eggOne = await eggContract.hatch({from: web3.eth.accounts[1]})
-    let eggTwo = await eggContract.hatch({from: web3.eth.accounts[1]})
-    let eggThree = await eggContract.hatch({from: web3.eth.accounts[1]})
+    let eggOne = await eggContract.hatch(1, {from: web3.eth.accounts[1]})
+    let eggTwo = await eggContract.hatch(12, {from: web3.eth.accounts[1]})
+    let eggThree = await eggContract.hatch(36, {from: web3.eth.accounts[1]})
     console.log(eggOne, eggTwo, eggThree)
     console.log(`eggs hatched`)
   } catch (err) {
