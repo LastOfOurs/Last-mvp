6d0b8cdd94e3849b51ef2cde6fa6e265cf1b0b13
Pong Cheecharern <k.cheecharern@gmail.com>
2019-01-12 16:44:54 +0700

    added basic identity proxy contract

--- Last-contracts/migrations/2_deploy_contract.1.js
+++ Last-contracts/migrations/2_deploy_contract.1.js
@@ -1,7 +1,9 @@
-var Last = artifacts.require('./Last.sol')
-var Egg = artifacts.require('./Egg.sol')
+const Last = artifacts.require('./Last.sol')
+const Egg = artifacts.require('./Egg.sol')
+const IdentityProxy = artifacts.require('./IdentityProxy.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(Last, 'Last', 'LTK', { gas: 5000000 })
   deployer.deploy(Egg)
+  deployer.deploy(IdentityProxy)
 }
