33fafeba930483ba34cc75c83f0ff00ced6eca42
Pong Cheecharern <k.cheecharern@gmail.com>
2019-01-15 22:47:46 +0700

    created a minimal relayer service

---
+++ Last-services/Relayer/src/index.js
@@ -0,0 +1,25 @@
+//exposes POST api
+//on POST - Send 
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const relayTx = require('./relayer/relayTx')
+
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+app.post('/api/v1/egg-tx', async (req, res) => {
+  //relay the transaction
+  console.log("got request")
+  let transaction = await relayTx(
+    req.body.eggAddress, 
+    req.body.proxyAddress, 
+    req.body.functionName,
+    req.body.functionTypes,
+    req.body.functionParams
+  )
+  console.log(transaction)
+  res.send(transaction)
+})
+
+app.listen(3000, () => console.log(`Relayer listening on port 3000!`))
---
+++ Last-services/Relayer/src/relayer/relayTx.js
@@ -0,0 +1,37 @@
+//relay signed message transaction
+const Web3 = require('web3')
+const lightwallet = require('eth-signer')
+const contract = require('truffle-contract')
+const fs = require('nano-fs')
+const IdentityJson = fs.readFileSync('../../../Last-contracts/build/contracts/IdentityProxy.json', 'utf8')
+const IdentityArtifacts = JSON.parse(IdentityJson)
+const Identity = contract(IdentityArtifacts)
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+Identity.setProvider(web3.currentProvider)
+
+//TODO: find a less hacky way to make truffle-deploy library work nicely
+if (typeof Identity.currentProvider.sendAsync !== 'function') {
+  Identity.currentProvider.sendAsync = function () {
+    return Identity.currentProvider.send.apply(
+      Identity.currentProvider, arguments
+    )
+  }
+}
+
+//address of Gas service
+let fromAccount = "0xe569d1f9dbe2bf342ea544a38ce9fe23b26beea2"
+
+async function relayTx(
+  destAddr, 
+  proxy, 
+  funcName,
+  typeArr,
+  dataArr) {
+  const LastIdentityContract = await Identity.deployed()
+  let data = lightwallet.txutils._encodeFunctionTxData(funcName,typeArr,dataArr)
+  // call forward request to a smart contract
+  await LastIdentityContract.forward(destAddr, 0, '0x' + data, {from: fromAccount})
+}
+
+module.exports = relayTx
